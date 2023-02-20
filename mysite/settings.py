500967a38e4608fa172e458aa0158a9a0ae35ece
Mason Galat <mason4work@outlook.com>
2023-02-19 21:04:51 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/STATIC_URL = '/static/'
 
 
-STATIC_URL = '/static/'
+STATIC_URL = './static/'
 
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:
